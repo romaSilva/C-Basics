c6974cc4b8e18cc53f4badabfd6e86187f9bb946
Romario Da Silva <roma.dasilva94@gmail.com>
2020-10-25 14:47:30 -0300

    Third lesson

---
+++ 03_instrucoes/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace _03_instrucoes
+{
+    class Program
+    {
+        static void Declaracoes()
+        {
+            int a;
+            int b = 2, c = 3;
+            const int d = 4;
+            a = 1;
+            Console.WriteLine(a + b + c + d);
+        }
+
+        static void InstrucaoIf(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nenhum argumento");
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine("Um argumento");
+            }
+            else
+            {
+                Console.WriteLine($"{args.Length} argumentos");
+            }
+        }
+
+        static void InstrucaoSwitch(string[] args)
+        {
+            int numeroDeArgumentos = args.Length;
+            switch (numeroDeArgumentos)
+            {
+                case 0:
+                    Console.WriteLine("Nenhum argumento");
+                    break;
+                case 1:
+                    Console.WriteLine("Um argumento");
+                    break;
+                default:
+                    Console.WriteLine($"{numeroDeArgumentos} argumentos");
+                    break;
+            }
+        }
+
+        static void InstrucaoWhile(string[] args)
+        {
+            int i = 0;
+            while (i < args.Length)
+            {
+                Console.WriteLine(args[i]);
+                i++;
+            }
+        }
+
+        static void InstrucaoDo()
+        {
+            string texto;
+            do
+            {
+                texto = Console.ReadLine();
+                Console.WriteLine(texto);
+            } while (!string.IsNullOrEmpty(texto));
+        }
+
+        static void InstrucaoFor(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+
+        static void InstrucaoForeach(string[] args)
+        {
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static void InstrucaoBreak()
+        {
+            while (true)
+            {
+                string s = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(s)) break;
+
+                Console.WriteLine(s);
+
+            }
+        }
+
+        static void InstrucaoContinue(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("/")) continue;
+
+                Console.WriteLine(args[i]);
+            }
+        }
+
+
+        static void Main()
+        {
+            string[] nomes = { "Caio", "Romario", "/Marcia", "Paulo" };
+
+            // Declaracoes();
+            // InstrucaoIf(nomes);
+            // InstrucaoSwitch(nomes);
+            // InstrucaoWhile(nomes);
+            // InstrucaoDo();
+            // InstrucaoFor(nomes);
+            // InstrucaoForeach(nomes);
+            // InstrucaoBreak();
+            InstrucaoContinue(nomes);
+
+        }
+
+    }
+}
